7d2040540274c6b555c05d8d3cbb9b500c7c01f6
IlyesCEO <ilyesmaro5@gmail.com>
2022-07-20 08:33:40 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -39,14 +39,5 @@ export default function handler(req, res) {
     jwt: {
       secret: 'GOCSPXweferewfwefwefewhtrmkytjlmtyjlkm76otykjotymjuj-V',
     },
-    callbacks: {
-      async redirect({ url, baseUrl }) {
-        // Allows relative callback URLs
-        if (url.startsWith('/')) return `${baseUrl}${url}`;
-        // Allows callback URLs on the same origin
-        else if (new URL(url).origin === baseUrl) return url;
-        return baseUrl;
-      },
-    },
   });
 }
