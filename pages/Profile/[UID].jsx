aaf343b99de6a581a1ae69f107787a758e1814cb
IlyesCEO <ilyesmaro5@gmail.com>
2022-07-20 06:49:52 +0100

    update1

--- pages/Profile/[UID].jsx
+++ pages/Profile/[UID].jsx
@@ -6,7 +6,7 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const Profile = ({ user }) => {
-  console.log(user);
+  console.log(JSON.parse(user));
   return (
     <div className="flex flex-col  gap-12 bg-slate-200 w-full min-h-screen">
       <Navbar currUser={user} />
@@ -14,23 +14,23 @@ const Profile = ({ user }) => {
         <Image
           width={160}
           height={160}
-          src={user.image}
+          src={JSON.parse(user).image}
           className=" rounded-full  "
           alt=""
         />
         <div className="flex gap-10 flex-col items-start justify-center">
           <div className="flex flex-row gap-4">
             <h2>Name</h2>
-            <p>{user.name}</p>
+            <p>{JSON.parse(user).name}</p>
           </div>
           <div className="flex flex-row gap-4">
             <h2>Email</h2>
-            <p>{user.email}</p>
+            <p>{JSON.parse(user).email}</p>
           </div>
           <div className="flex flex-row items-center justify-center gap-4">
             <h2>Verified email</h2>
-            <p>{user.emailVerified ? 'True' : 'False'}</p>
-            {!user.emailVerified && (
+            <p>{JSON.parse(user).emailVerified ? 'True' : 'False'}</p>
+            {!JSON.parse(user).emailVerified && (
               <button className="bg-green-600 p-3 rounded-md">
                 Verify Now
               </button>
@@ -48,17 +48,11 @@ const Profile = ({ user }) => {
 export async function getServerSideProps(context) {
   console.log('this is the profile server log');
   console.log(context.query);
-  const { UID } = context.query;
-  const prisma = new PrismaClient();
-  const user = await prisma.user.findUnique({
-    where: {
-      id: UID,
-    },
-  });
-
+  const { id } = JSON.parse(context.query.user);
+  console.log(JSON.parse(context.query.user));
   return {
     props: {
-      user,
+      user: JSON.parse(context.query.user),
     },
   };
 }
