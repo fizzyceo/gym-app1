cf1244189296634e9834022cb35f46f2766450ad
fizzyceo <84289623+fizzyceo@users.noreply.github.com>
2022-07-25 21:12:32 +0100

    Update [UID].jsx

--- pages/Profile/[UID].jsx
+++ pages/Profile/[UID].jsx
@@ -6,7 +6,7 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const Profile = ({ user }) => {
-  console.log(JSON.parse(user));
+  
   return (
     <div className="flex flex-col  gap-12 bg-slate-200 w-full min-h-screen">
       <Navbar currUser={user} />
@@ -14,23 +14,23 @@ const Profile = ({ user }) => {
         <Image
           width={160}
           height={160}
-          src={JSON.parse(user).image}
+          src={user.image}
           className=" rounded-full  "
           alt=""
         />
         <div className="flex gap-10 flex-col items-start justify-center">
           <div className="flex flex-row gap-4">
             <h2>Name</h2>
-            <p>{JSON.parse(user).name}</p>
+            <p>{user.name}</p>
           </div>
           <div className="flex flex-row gap-4">
             <h2>Email</h2>
-            <p>{JSON.parse(user).email}</p>
+            <p>{user.email}</p>
           </div>
           <div className="flex flex-row items-center justify-center gap-4">
             <h2>Verified email</h2>
-            <p>{JSON.parse(user).emailVerified ? 'True' : 'False'}</p>
-            {!JSON.parse(user).emailVerified && (
+            <p>{user.emailVerified ? 'True' : 'False'}</p>
+            {!user.emailVerified && (
               <button className="bg-green-600 p-3 rounded-md">
                 Verify Now
               </button>
@@ -48,11 +48,11 @@ const Profile = ({ user }) => {
 export async function getServerSideProps(context) {
   console.log('this is the profile server log');
   console.log(context.query);
-  const { id } = JSON.parse(context.query.user);
-  console.log(JSON.parse(context.query.user));
+  const user = JSON.parse(context.query.user);
+  console.log(user);
   return {
     props: {
-      user: JSON.parse(context.query.user),
+      user: user,
     },
   };
 }
